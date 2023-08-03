74369cd15ddb096eea1713b4f54f4776981b3b41
Rashen <rashen598@gmail.com>
2023-08-03 11:59:34 +0530

    updated the add customer to mysql.

--- src/edu/icet/controller/CustomerController.java
+++ src/edu/icet/controller/CustomerController.java
@@ -1,9 +1,14 @@
 package edu.icet.controller;
 
+import edu.icet.db.DBConnection;
 import edu.icet.model.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CustomerController {
 
@@ -25,5 +30,23 @@ public class CustomerController {
         double salary = Double.parseDouble(txtSalary.getText());
         Customer customer = new Customer(id,name,address,salary);
         System.out.println(customer);
+
+        String SQL = "Insert into Customer Values(?,?,?,?)";
+
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement pst = connection.prepareStatement(SQL);
+            pst.setObject(1,customer.getId());
+            pst.setObject(2,customer.getName());
+            pst.setObject(3,customer.getAddress());
+            pst.setObject(4,customer.getSalary());
+            int i = pst.executeUpdate();
+            System.out.println(i > 0 ? "Added" : "Failed");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
