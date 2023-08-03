585f29457db29b89fb88ae20149053e532e56f96
Rashen <rashen598@gmail.com>
2023-08-03 07:59:21 +0530

    Initiated the project.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
