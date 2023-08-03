ef1bc75986053aef85696dfeca09f008f9b651e1
Rashen <rashen598@gmail.com>
2023-08-03 11:47:28 +0530

    updated the DBConnection class.

---
+++ src/edu/icet/db/DBConnection.java
@@ -0,0 +1,20 @@
+package edu.icet.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+    private DBConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/Thogakade","root","Rashen1234");
+    }
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return dbConnection == null ? dbConnection = new DBConnection() : dbConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
